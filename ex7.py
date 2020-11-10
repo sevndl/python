61ba51d9036f3ae157dd39ca516a1581f03013be
Severin Nandillon <nandillons@gmail.com>
2020-11-10 11:30:54 +0100

    Exercice 7 b) : retourner vrai si une string est un palindrome

--- ex7.py
+++ ex7.py
@@ -1,5 +1,13 @@
 def inverser(phrase):
   print(''.join(reversed(phrase)));
-  return 0;
+
+def palindrome(phrase):
+  nouvellePhrase = ''.join(reversed(phrase));
+  if nouvellePhrase == phrase:
+    print("true");
+  else:
+    print("false");
 
 inverser("bonjour");
+palindrome("bonjour");
+palindrome("radar");
