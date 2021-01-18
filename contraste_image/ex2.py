b2b890836db4043cd40b9449e1ba0213ded2626a
Severin Nandillon <nandillons@gmail.com>
2021-01-18 10:17:04 +0100

    Update ex2.py

--- contraste_image/ex2.py
+++ contraste_image/ex2.py
@@ -7,9 +7,17 @@ def nbColonnes(m):
 def calculer_luminosite(m):
   print("La matrice à traiter est : ", m)
   sum = 0
-  for x in m:
-    for y in x:
-      sum += y
-  return int(sum / (nbLignes(m) * nbColonnes(m)))
+  for x in range(0, nbLignes(m)):
+    for y in range(0, nbColonnes(m)):
+      sum += m[x][y]
+  return sum // (nbLignes(m) * nbColonnes(m))
+
+# def calculer_luminosite(m):
+#   print("La matrice à traiter est : ", m)
+#   sum = 0
+#   for x in m:
+#     for y in x:
+#       sum += y
+#   return sum // (nbLignes(m) * nbColonnes(m))
 
 print("La luminosité de la matrice est : ", calculer_luminosite([[0,0,100,50], [0,0,100,100], [20,35,50,75]]))
