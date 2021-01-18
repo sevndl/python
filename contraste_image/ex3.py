780fed0f23823ac4f2d2a1bc1a3cd87ce9fb8e2a
Severin Nandillon <nandillons@gmail.com>
2021-01-18 10:41:41 +0100

    Update ex3.py

--- contraste_image/ex3.py
+++ contraste_image/ex3.py
@@ -1,13 +1,29 @@
+def nbL(m):
+  return len(m)
+
+def nbC(m):
+  return len(m[0])
+
 def calculer_contraste(m, luminosite):
-  for x in range(0, len(m)):
-    for y in range(0, len(m[0])):
+  mContraste = []
+  for i in range(0, nbL(m)):
+    mContraste.append([])
+  for x in range(0, nbL(m)):
+    for y in range(0, nbC(m)):
       if m[x][y] < luminosite:
-        m[x][y] //= 2
+        mContraste[x].append(m[x][y] // 2)
+        # m[x][y] //= 2
       else:
-        m[x][y] *= 2
-        if m[x][y] > 100:
-          m[x][y] = 100
+        mContraste[x].append(m[x][y] * 2)
+        # m[x][y] *= 2
+        if mContraste[x][y] > 100:
+          mContraste[x][y] = 100
+  print("Matrice initiale :")
   for ligne in m:
+    print(ligne)
+  print()
+  print("Matrice contraste :")
+  for ligne in mContraste:
     print(ligne)
 
 print(calculer_contraste([[0,0,100,50], [0,0,100,100], [20,35,50,75]], 44))
