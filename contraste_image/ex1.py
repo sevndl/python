f38d6e7c6687cd4034cc74b63f86e03a2dd218da
Severin Nandillon <nandillons@gmail.com>
2021-01-18 09:47:38 +0100

    code prof

--- contraste_image/ex1.py
+++ contraste_image/ex1.py
@@ -4,3 +4,24 @@ def ecrire_matrice(m):
     print(x)
 
 print(ecrire_matrice([[1,2,3], [4,5,6], [7,8,9]]))
+
+
+# def nbLignes(M):
+#   return len(M)
+
+# def nbColonnes(M):
+#   return len(M[0])
+
+# def initialiserMatrice(lignes, colonnes=0, valeur=0):
+#   if colonnes==0:
+#     # on a affaire à une matrice carrée lignes x lignes
+#     colonnes=lignes
+#   matrice= []
+#   for iLig in range(0,lignes):
+#     # ajouter (append) dans matrice une ligne avec colonnes valeurs
+#     uneLigne=[]
+#   for iCol in range(0,colonnes):
+#     uneLigne.append(valeur)
+#   matrice.append(uneLigne)
+
+#   return matrice
