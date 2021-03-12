4ab20a63a625604bd797206fba83ada02caadde2
sevndl <nandillons@gmail.com>
2021-03-12 13:20:21 +0100

    Affichage des valeurs dans la grille

--- sudoku/main.py
+++ sudoku/main.py
@@ -10,8 +10,9 @@ tailleCanvas = (tailleGrille * tailleCase) + 2
 hauteurMainWindow = tailleCanvas + 100
 largeurMainWindow = tailleCanvas + 100
 
-# grille.setValeur(1, 1, 8)
-# grille.setValeur(4, 7, 2)
+grille.setValeur(1, 1, 8)
+grille.setValeur(4, 7, 2)
+grille.setValeur(5, 7, 6)
 
 # Fonction d'affichage de la grille
 # Ligne plus épaisse tous les (multiples de la
@@ -36,6 +37,19 @@ def affichageGrille():
       width = 2 if (y % math.sqrt(tailleGrille) == 0) else 1
     )
 
+# Fonction d'affichage des valeurs dans la grille
+def affichageValeurs():
+  for x in range(1, tailleGrille + 1):
+    for y in range(1, tailleGrille + 1):
+      valeur = grille.getValeur(x, y)
+      if valeur > 0:
+        playGround.create_text(
+          ((x * tailleCase) - (tailleCase / 2)) + 4 ,
+          ((y * tailleCase) - (tailleCase / 2)) + 4,
+          fill = "black",
+          text = valeur
+        )
+
 # Fenêtre principale
 mainWindow = Tk()
 mainWindow.title("Projet sudoku en python")
@@ -44,7 +58,7 @@ mainWindow.minsize(width = largeurMainWindow, height = hauteurMainWindow)
 # Canvas du plateau de jeu
 playGround = Canvas(
   mainWindow,
-  bg = 'light grey',
+  bg = 'light blue',
   height = tailleCanvas,
   width = tailleCanvas
 )
@@ -52,6 +66,7 @@ playGround.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 
 # Affichage de la grille
 affichageGrille()
+affichageValeurs()
 
 # Affichage de la fenêtre principale,
 # et intrinsèquement des éléments graphiques la composant
